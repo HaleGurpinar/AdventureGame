43880fa59df27e6d848e0f1c6ccf5ba76cda8184
HaleGurpinar <106753721+HaleGurpinar@users.noreply.github.com>
2023-04-04 21:54:38 +0300

    Player - Inventory Class

---
+++ src/com/patika/Inventory.java
@@ -0,0 +1,13 @@
+package com.patika;
+
+public class Inventory {
+
+    private boolean water;
+    private boolean food;
+    private boolean firewood;
+    private  String weaponName;
+    private String armorName;
+    private int weaponDamage;
+    private int armorDefence;
+
+}
---
+++ src/com/patika/Player.java
@@ -0,0 +1,13 @@
+package com.patika;
+
+public class Player {
+
+    private int damage;
+    private int health;
+    private int money;
+    private String name;
+    Inventory inventory;
+
+    public void selectChar(){}
+
+}
